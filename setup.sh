f3e878a4a33d45460f2923390b1c1b2d99d826b9
Nina Zakharenko <nzakharenko@gmail.com>
2018-11-08 00:35:27 -0800

    fix magenta color

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ red=$(tput setaf 1)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 blue=$(tput setaf 4)
-magenta =$(tput setaf 5)
+magenta=$(tput setaf 5)
 cyan=$(tput setaf 6)
 white=$(tput setaf 7)
 
